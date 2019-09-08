7b6435ab063008762117984c1a9a8353e2b6bfab
Tarun Gorai <goraitarunkumar@gmail.com>
2019-09-08 15:29:37 +0530

    play button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cmp-databining-assignment';
+  onIntervalFired(firedNumber: number) {
+    console.log({firedNumber});
+  }
 }
